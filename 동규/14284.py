23408de558066e86166db538a2c2a89a3c076129
DonggyuJin <97138841+DonggyuJin@users.noreply.github.com>
2023-05-31 14:06:11 +0900

    [동규] 14284번: 간선 이어가기 2

---
+++ 동규/14284.py
@@ -0,0 +1,36 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+INF = int(1e9)
+distance = [INF] * (n + 1)
+
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+
+start, end = map(int, input().split())
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, node = heapq.heappop(q)
+        if distance[node] < dist:
+            continue
+        for next in graph[node]:
+            cost = distance[node] + next[1]
+            if cost < distance[next[0]]:
+                distance[next[0]] = cost
+                heapq.heappush(q, (cost, next[0]))
+
+
+dijkstra(start)
+
+print(distance[end])
